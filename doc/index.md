9e66ca828cbba9d19c43c9e501898cd54f1e1ad6
Krzysztof Jusiak <krzysztof@jusiak.net>
2016-01-19 18:45:42 +0000

    [doc] run this code

--- doc/index.md
+++ doc/index.md
@@ -5,3 +5,5 @@ msm-lite: C++14 Meta State Machine Library
 
 > Yours scalable C++14 header only eUML-like meta state machine library with no dependencies
 
+![YOUTUBE](LRLQE2N0DKc)
+
--- doc/js/gitter.js
+++ doc/js/gitter.js
@@ -1,8 +1,4 @@
 ((window.gitter = {}).chat = {}).options = {
   room: 'krzysztof-jusiak/msm-lite'
 };
-$.getScript('https://sidecar.gitter.im/dist/sidecar.v1.js', function()
-{
-    // script is now loaded and executed.
-    // put your dependent JS here.
-});
+$.getScript('https://sidecar.gitter.im/dist/sidecar.v1.js', function() { });
