cdcf20bb24d2787944e6a241df0d660db882f5be
Krzysztof Jusiak <krzysztof@jusiak.net>
2016-01-20 13:24:01 +0000

    doc

--- doc/js/gitter.js
+++ doc/js/gitter.js
@@ -1,5 +1,4 @@
 ((window.gitter = {}).chat = {}).options = {
   room: 'krzysztof-jusiak/msm-lite'
- ,showChatByDefault: true
 };
 $.getScript('https://sidecar.gitter.im/dist/sidecar.v1.js', function() { });
