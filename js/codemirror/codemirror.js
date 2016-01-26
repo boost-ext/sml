1489d43fbbbe0f0af311984a369e69d243f0d318
Krzysztof Jusiak <krzysztof@jusiak.net>
2016-01-26 13:55:55 +0000

    wknd

--- js/codemirror/codemirror.js
+++ js/codemirror/codemirror.js
@@ -10,8 +10,8 @@
 (function(mod) {
   if (typeof exports == "object" && typeof module == "object") // CommonJS
     module.exports = mod();
-  else if (typeof define == "function" && define.amd) // AMD
-    return define([], mod);
+  else if (typeof define_wknd == "function")
+    return define_wknd([], mod);
   else // Plain browser env
     this.CodeMirror = mod();
 })(function() {
