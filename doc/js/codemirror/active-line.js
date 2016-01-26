c622e80f56ef770bc56704823d72536fba026793
Krzysztof Jusiak <krzysztof@jusiak.net>
2016-01-26 13:50:28 +0000

    wknd for requirejs and codemirror

--- doc/js/codemirror/active-line.js
+++ doc/js/codemirror/active-line.js
@@ -10,8 +10,8 @@
 (function(mod) {
   if (typeof exports == "object" && typeof module == "object") // CommonJS
     mod(require("../../lib/codemirror"));
-  else if (typeof define == "function" && define.amd) // AMD
-    define(["../../lib/codemirror"], mod);
+  else if (typeof define_wknd == "function")
+    define_wknd(["../../lib/codemirror"], mod);
   else // Plain browser env
     mod(CodeMirror);
 })(function(CodeMirror) {
