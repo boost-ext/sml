45c245597abfcde413048c9a0c1ce0a9a0169288
Krzysztof Jusiak <krzysztof@jusiak.net>
2016-01-30 14:04:45 +0000

    [doc] gitter link

--- doc/boost/js/gitter.js
+++ doc/boost/js/gitter.js
@@ -1,4 +1,4 @@
 ((window.gitter = {}).chat = {}).options = {
-  room: 'krzysztof-jusiak/msm-lite'
+  room: 'boost-experimental/msm-lite'
 };
 $.getScript('https://sidecar.gitter.im/dist/sidecar.v1.js', function() { });
