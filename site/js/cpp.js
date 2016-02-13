dd058b12dd574f971a56ef0827cda4d35b2ec7fb
Continuous Integration <krzysztof@jusiak.net>
2016-02-13 22:25:36 +0000

    doc update

--- site/js/cpp.js
+++ site/js/cpp.js
@@ -34,6 +34,7 @@ function cpp(id, file, text) {
     var compile_txt = document.createTextNode("Compile & Run (Ctrl+Enter)");
     compile_btn.setAttribute("id", "compile_and_run_" + id);
     compile_btn.setAttribute("class", "btn btn-neutral float-right");
+    compile_btn.setAttribute("style", "position: relative; top: 60px; right: 20px; z-index: 10;");
     compile_btn.appendChild(compile_txt);
     compile_btn.onclick = function() { compile_and_run(id) };
 
@@ -74,9 +75,9 @@ function compile_and_run(id) {
               "file" : "boost/msm-lite.hpp"
             , "code" : get_cpp_file("https://raw.githubusercontent.com/boost-experimental/msm-lite/master/include/boost/msm-lite.hpp")
            }]
-       , "options": "warning,cpp-pedantic-errors,optimize,boost-nothing,c++1y"
-       , "compiler" : "clang-head"
-       , "compiler-option-raw": "-I." + "\n" + "-fno-color-diagnostics"
+         , "options": "warning,cpp-pedantic-errors,optimize,boost-nothing,c++1y"
+         , "compiler" : "clang-head"
+         , "compiler-option-raw": "-I." + "\n" + "-fno-color-diagnostics"
     }));
 }
 
@@ -103,7 +104,7 @@ function get_example(id, file) {
     cpp_output[id].setOption("theme", 'mdn-like');
 
     cpp_code[id].setValue(get_cpp_file(file));
-	  cpp_code[id].setCursor(Math.floor(cpp_code[id].lineCount() - (cpp_code[id].lineCount() / 3)) , 0);
+    cpp_code[id].setCursor(Math.floor(cpp_code[id].lineCount() - (cpp_code[id].lineCount() / 3)) , 0);
 }
 
 Element.prototype.remove = function() {
@@ -118,33 +119,114 @@ NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
     }
 }
 
+function cleanup(str) {
+  text = str.replace(/[\n]{3,}/g, "\n").slice(0, -1);
+  var lines = text.split('\n');
+  var result = '';
+  for(var i = 0; i < lines.length; i++) {
+    if (!i && !lines[i].length) {
+        continue;
+    }
+    result += lines[i] + "\n";
+  }
+  return result;
+}
+
 $(document).ready(function () {
     $('img[alt="CPP"]').each(function () {
-        var file = $(this).attr('src');
-        var basename = $(this).attr('src').split('/')[$(this).attr('src').split('/').length - 1];
-		var regex = "#include.*";
-		var example = get_cpp_file(file);
-		var i = example.lastIndexOf("#include")
-		var n = example.substring(i).indexOf('\n');
-		var id = gid++;
-		var compile = "\/\/ $CXX -std=c++14 " + basename;
-		example = $('<div/>').text(example.substring(i + n + 2)).html();
-        $(this).replaceWith('<button class="btn btn-neutral float-right" id="run_it_btn_' + id + '" onclick="cpp(' + id + ', \'' + file + '\', \'Run this code!\')">Run this code!</button><textarea style="display: none" id="code_' + id + '"></textarea><br /><textarea style="display: none" id="output_' + id + '"></textarea><div id="code_listing_' + id + '"><pre><code class="cpp">' + compile + '\n\n' + example + '</code></pre></div>');
+      var file = $(this).attr('src');
+      var basename = $(this).attr('src').split('/')[$(this).attr('src').split('/').length - 1];
+      var begin = "//<-";
+      var end = "//->";
+      var example = get_cpp_file(file);
+      var lines = example.split('\n');
+      var example_result = '';
+      var ignored = false;
+      var tmp_ignored = false;
+      for(var i = 0; i < lines.length; ++i){
+        var line = lines[i];
+        if (line.indexOf(begin) != -1) {
+          ignored = true;
+          tmp_ignored = true;
+        } else if (line.indexOf(end) != -1) {
+          tmp_ignored = false;
+        }
+        if (!ignored && line.trim().indexOf("//") != 0) {
+          example_result += line + '\n';
+        }
+        ignored = tmp_ignored;
+      }
+      var id = gid++;
+      var compile = "\/\/ $CXX -std=c++14 " + basename;
+      example = $('<div/>').text(example_result.replace(/[\n]{3,}/g, "\n")).html();
+      $(this).replaceWith('<button style="position: relative; top: 60px; right: 20px; z-index: 10;" class="btn btn-neutral float-right" id="run_it_btn_' + id + '" onclick="cpp(' + id + ', \'' + file + '\', \'Run this code!\')">Run this code!</button><textarea style="display: none" id="code_' + id + '"></textarea><br /><textarea style="display: none" id="output_' + id + '"></textarea><div id="code_listing_' + id + '"><pre><code class="cpp">' + compile + '\n' + example + '</code></pre></div>');
     });
 
     $('img[alt="CPP(BTN)"]').each(function () {
-        var text = $(this).attr('src');
-        console.log(text);
-        var name = text.split("|")[0].replace(/_/g, ' ').replace(/\//g, '').replace(/\./g, '');
-        var file = text.split("|")[1];
-        var basename = $(this).attr('src').split('/')[$(this).attr('src').split('/').length - 1];
-		var regex = "#include.*";
-		var example = get_cpp_file(file);
-		var i = example.lastIndexOf("#include")
-		var n = example.substring(i).indexOf('\n');
-		var id = gid++;
-		var compile = "\/\/ $CXX -std=c++14 " + basename;
-		example = $('<div/>').text(example.substring(i + n + 2)).html();
-        $(this).replaceWith('<table class="float-left"><tr><td><button class="btn" id="run_it_btn_' + id + '" onclick="cpp(' + id + ', \'' + file + '\', \'' + name + '\')">' + name + '</button><textarea style="display: none" id="code_' + id + '"></textarea><br /><textarea style="display: none" id="output_' + id + '"></textarea><div style=""id="code_listing_' + id + '"></div></td></tr></table>');
+      var text = $(this).attr('src');
+      console.log(text);
+      var name = text.split("|")[0].replace(/_/g, ' ').replace(/\//g, '').replace(/\./g, '');
+      var file = text.split("|")[1];
+      var basename = $(this).attr('src').split('/')[$(this).attr('src').split('/').length - 1];
+      var regex = "#include.*";
+      var example = get_cpp_file(file);
+      var i = example.lastIndexOf("#include")
+      var n = example.substring(i).indexOf('\n');
+      var id = gid++;
+      var compile = "\/\/ $CXX -std=c++14 " + basename;
+      example = $('<div/>').text(example.substring(i + n + 2)).html();
+      $(this).replaceWith('<table class="float-left"><tr><td><button class="btn" id="run_it_btn_' + id + '" onclick="cpp(' + id + ', \'' + file + '\', \'' + name + '\')">' + name + '</button><textarea style="display: none" id="code_' + id + '"></textarea><br /><textarea style="display: none" id="output_' + id + '"></textarea><div style=""id="code_listing_' + id + '"></div></td></tr></table>');
+    });
+
+    $('img[alt="CPP(SPLIT)"]').each(function () {
+      var file = $(this).attr('src');
+      var basename = $(this).attr('src').split('/')[$(this).attr('src').split('/').length - 1];
+      var name = basename.replace(".cpp", "").replace(/_/g, " ");
+      name = name.charAt(0).toUpperCase() + name.substring(1);
+      var begin = "//<-";
+      var end = "//->";
+      var example = get_cpp_file(file);
+      var lines = example.split('\n');
+      var example_result = '';
+      var test_result = '';
+      var ignored = 0;
+      var tmp_ignored = 0;
+      for(var i = 0; i < lines.length; i++){
+        var line = lines[i];
+        if (line.indexOf(begin) != -1) {
+          ignored = 1;
+          tmp_ignored = 1;
+        } else if (line.indexOf(end) != -1) {
+          tmp_ignored = 0;
+        }
+        if (!ignored && line.indexOf("int main()") == 0) {
+          if (example_result.replace(/\n/g, "") == "") {
+            tmp_ignored = 3;
+          } else {
+            tmp_ignored = 2;
+          }
+          ignored = 1;
+        } else if (ignored == 3 && !line.length) {
+          tmp_ignored = 2;
+          ignored = 1;
+        }
+        if (line.trim().indexOf("//") != 0) {
+          if (!ignored) {
+            example_result += line + '\n';
+          } else if (ignored == 2 && line.indexOf("}") != 0) {
+            test_result += line + '\n';
+          } else if (ignored == 3) {
+            example_result += line + '\n';
+          }
+        }
+        ignored = tmp_ignored;
+      }
+      var id = gid++;
+      var ex = cleanup(example_result);
+      var ts = cleanup(test_result);
+      var height = Math.max(ex.split("\n").length, ts.split("\n").length) * 25;
+      example = $('<div/>').text(ex).html();
+      test = $('<div/>').text(ts).html();
+      $(this).replaceWith('<table style="table-layout: fixed; border-collapse:collapse; padding:0; height: 30px; width: 100%; border: 1px;"><thead><tr><th>' + name + '</th><th><button class="btn btn-neutral" id="run_it_btn_' + id + '" onclick="cpp(' + id + ', \'' + file + '\', \'Test this code!\')">Test this code!</button><div id="code_listing_' + id + '"></th></tr></thead><tbody><tr><td><pre><code class="cpp" style="height: ' + height + 'px;">' + example + '</code></pre></td><td><pre><code class="cpp" style="height: ' + height + 'px;">' + test + '</code></pre></td></tr></tbody></table><textarea style="display: none" id="code_' + id + '"></textarea><br /><textarea style="display: none" id="output_' + id + '"></textarea></div>');
     });
 });
