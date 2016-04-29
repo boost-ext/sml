e99a31eacc68a9cb352c4e4d26bb61cc0be33fc7
Continuous Integration <krzysztof@jusiak.net>
2016-04-29 12:16:00 +0000

    doc update

--- boost/themes/boost-experimental/js/gitter.js
+++ boost/themes/boost-experimental/js/gitter.js
@@ -1,4 +1,4 @@
 ((window.gitter = {}).chat = {}).options = {
-  room: 'boost-experimental/di'
+  room: 'boost-experimental/msm-lite'
 };
 $.getScript('https://sidecar.gitter.im/dist/sidecar.v1.js', function() { });
