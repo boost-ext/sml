92eb6eb2b388e1015b39cb0f3ef49fe5156a5457
David Feltell <dave@feltell.net>
2016-09-06 22:07:13 +0100

    Fix indentation

--- test/ft_logging.cpp
+++ test/ft_logging.cpp
@@ -6,9 +6,9 @@ std::vector<std::string> messages_out;
 
 template <class SM, class TEvent>
 void log_process_event(const TEvent& evt) {
-	std::stringstream sstr;
-	sstr << evt.c_str();
-	messages_out.push_back(sstr.str());
+  std::stringstream sstr;
+  sstr << evt.c_str();
+  messages_out.push_back(sstr.str());
 }
 
 
@@ -24,9 +24,9 @@ void log_action(const TAction&, const TEvent&) {
 
 template <class SM, class TSrcState, class TDstState>
 void log_state_change(const TSrcState& src, const TDstState& dst) {
-	std::stringstream sstr;
-	sstr << src.c_str() << " -> " << dst.c_str();
-	messages_out.push_back(sstr.str());
+  std::stringstream sstr;
+  sstr << src.c_str() << " -> " << dst.c_str();
+  messages_out.push_back(sstr.str());
 }
 
 
@@ -37,20 +37,20 @@ void log_state_change(const TSrcState& src, const TDstState& dst) {
 namespace msm = boost::msm::lite;
 
 struct e2 {
-	static auto c_str() { return "An Event"; }
+  static auto c_str() { return "An Event"; }
 };
 struct s1_label {
-	static auto c_str() { return "A State"; }
+  static auto c_str() { return "A State"; }
 };
 auto s1 = msm::state<s1_label>{};
 
 test logging = [] {
   messages_out.clear();
   std::vector<std::string> messages_expected = {
-	  "e1",
-	  "idle -> A State",
-	  "An Event",
-	  "A State -> terminate"
+    "e1",
+    "idle -> A State",
+    "An Event",
+    "A State -> terminate"
   };
   struct c {
     auto configure() noexcept {
@@ -76,14 +76,14 @@ test logging = [] {
 test logging_entry_exit = [] {
   messages_out.clear();
   std::vector<std::string> messages_expected = {
-	  "e1",
-	  "on_exit",
-	  "idle -> A State",
-	  "on_entry",
-	  "An Event",
-	  "on_exit",
-	  "A State -> terminate",
-	  "on_entry"
+    "e1",
+    "on_exit",
+    "idle -> A State",
+    "on_entry",
+    "An Event",
+    "on_exit",
+    "A State -> terminate",
+    "on_entry"
   };
 
   struct c {
@@ -92,8 +92,8 @@ test logging_entry_exit = [] {
       // clang-format off
       return make_transition_table(
           *"idle"_s + "e1"_t = s1
-		, s1 + msm::on_entry / [](){}
-		, s1 + msm::on_exit / [](){}
+        , s1 + msm::on_entry / [](){}
+        , s1 + msm::on_exit / [](){}
         , s1 + event<e2> = X
       );
       // clang-format on
