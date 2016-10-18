e3a20c1ac7a45c4a76df5520c756cef7c20bc800
Krzysztof Jusiak <krzysztof@jusiak.net>
2016-10-18 16:02:02 +0100

    [fix] Initial states of composite regions can be entered/exited multiple times during initialization

--- test/ft/ft_orthogonal_regions.cpp
+++ test/ft/ft_orthogonal_regions.cpp
@@ -8,6 +8,7 @@
 #include <boost/msm-lite.hpp>
 #include <string>
 #include <utility>
+#include <vector>
 
 namespace msm = boost::msm::lite;
 
@@ -102,6 +103,44 @@ test orthogonal_regions_initial_entry = [] {
   msm::sm<c> sm{c_};
   expect(1 == c_.entry_1);
   expect(1 == c_.entry_2);
+};
+
+test orthogonal_regions_entry_exit_multiple = [] {
+  enum class calls { a_entry, a_exit, b_entry, b_exit, c_entry, c_exit };
+  struct c {
+    auto operator()() const noexcept {
+      using namespace msm;
+      const auto a = state<class A>;
+      const auto b = state<class B>;
+      const auto c = state<class C>;
+
+      //clang format off
+      return make_transition_table(
+        *"init1"_s = a,
+        *"init2"_s = b,
+        *"init3"_s = c,
+        a + msm::on_entry / [](std::vector<calls>& c) { c.push_back(calls::a_entry); },
+        a + msm::on_exit  / [](std::vector<calls>& c) { c.push_back(calls::a_exit); },
+        b + msm::on_entry / [](std::vector<calls>& c) { c.push_back(calls::b_entry); },
+        b + msm::on_exit  / [](std::vector<calls>& c) { c.push_back(calls::b_exit); },
+        c + msm::on_entry / [](std::vector<calls>& c) { c.push_back(calls::c_entry); },
+        c + msm::on_exit  / [](std::vector<calls>& c) { c.push_back(calls::c_exit); },
+        a + event<e1> = X,
+        b + event<e1> = X,
+        c + event<e1> = X
+      );
+      // clang-format on
+    }
+  };
+
+  std::vector<calls> c_;
+  msm::sm<c> sm{c_};
+  expect(std::vector<calls>{calls::a_entry, calls::b_entry, calls::c_entry} == c_);
+
+  c_.clear();
+  sm.process_event(e1{});
+  expect(std::vector<calls>{calls::a_exit, calls::b_exit, calls::c_exit} == c_);
+  expect(sm.is(msm::X, msm::X, msm::X));
 };
 
 test orthogonal_regions_entry_exit = [] {
