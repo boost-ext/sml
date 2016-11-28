dd2464fa49d746036dd21e65a60f8d7beecafa3b
Krzysztof Jusiak <krzysztof@jusiak.net>
2016-11-28 19:07:47 +0000

    [pph]

--- include/boost/sml.hpp
+++ include/boost/sml.hpp
@@ -6,7 +6,7 @@
 //
 #pragma once
 #if (__cplusplus < 201305L && _MSC_VER < 1900)
-#error "Boost.SML requires C++14 support (Clang-3.4+, GCC-5.1+, MSVC-2015+)"
+#error "[Boost].SML requires C++14 support (Clang-3.4+, GCC-5.1+, MSVC-2015+)"
 #else
 #define BOOST_SML_VERSION 1'1'0
 #define BOOST_SML_NAMESPACE_BEGIN \
