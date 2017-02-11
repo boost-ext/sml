9d530ce2d8b15ed82eaa87d0b7bc5035fe24f388
Kris Jusiak <kris@jusiak.net>
2017-02-11 23:11:44 +0000

    [doc] embo-2017 examples

---
+++ doc/embo-2017/example/connection_v3.cpp
@@ -0,0 +1,63 @@
+#include <cstdio>
+#include <variant>
+#include <type_traits>
+
+template<class ... Ts>
+struct overload : Ts... {
+  using Ts::operator()...;
+  template<class ... TArgs>
+  overload(TArgs && ... args) : TArgs(std::forward<TArgs>(args))... { }
+};
+
+template<class ... Ts>
+overload(Ts...) -> overload<std::decay_t<Ts>...>;
+
+struct Ping {};
+void resetTimeout() { std::puts("resetTimeout"); }
+void establish() { std::puts("establish"); }
+void close() { std::puts("close"); }
+bool is_valid(const Ping&) { return true; }
+
+class ConnectionV3 {
+    struct Disconnected { };
+    struct Connecting { };
+    struct Connected { };
+
+    std::variant<Disconnected, Connecting, Connected> state;
+
+public:
+    void connect() {
+        std::visit(overload{
+            [&](Disconnected) { establish(); state = Connecting{}; },
+            [&](Connected) { establish(); state = Connecting{}; },
+            [](auto) { }
+        }, state);
+    }
+
+    void disconnect() {
+        std::visit(overload{
+            [&](Connecting) { close(); state = Disconnected{}; },
+            [&](Connected) { close(); state = Disconnected{}; },
+            [](auto) { }
+        }, state);
+    }
+
+    void established() {
+        state = Connected{};
+    }
+
+    void ping(const Ping& event) {
+        if (std::get_if<Connected>(&state) && is_valid(event)) {
+            resetTimeout();
+        }
+    }
+
+    void timeout() { connect(); }
+};
+
+int main() {
+  ConnectionV3 connection;
+  static_assert(8 == sizeof(connection), "");
+
+  connection.connect();
+}
