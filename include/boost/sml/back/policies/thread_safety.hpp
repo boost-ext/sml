e79084704cbe897b2793809dcf8be825ad619d3e
Kris Jusiak <kris@jusiak.net>
2018-02-28 22:04:46 +0000

    :art: [back-end][policies] Refactor thread safe policy
    Problem:
    - Policies are coupled to the SM.
    
    Solution:
    - Extract policies implementation into the policy itself.

--- include/boost/sml/back/policies/thread_safety.hpp
+++ include/boost/sml/back/policies/thread_safety.hpp
@@ -12,11 +12,25 @@
 namespace back {
 namespace policies {
 
-struct thread_safety_policy__ {};
+struct thread_safety_policy__ {
+  auto create_lock() { return *this; }
+  __BOOST_SML_ZERO_SIZE_ARRAY(aux::byte);
+};
 
-template <class T>
-struct thread_safe : aux::pair<thread_safety_policy__, thread_safe<T>> {
-  using type = T;
+template <class TLock>
+struct thread_safe : aux::pair<thread_safety_policy__, thread_safe<TLock>> {
+  using type = thread_safe;
+
+  auto create_lock() {
+    struct lock_guard {
+      explicit lock_guard(TLock &lock) : lock_{lock} { lock_.lock(); }
+      ~lock_guard() { lock_.unlock(); }
+      TLock &lock_;
+    };
+    return lock_guard{lock};
+  }
+
+  TLock lock;
 };
 
 }  // policies
