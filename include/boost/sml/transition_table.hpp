23b96e5be756383729cea07ffb02ee760c1c7112
Kris Jusiak <kris@jusiak.net>
2019-01-06 10:55:11 -0700

    :wrench: :arrow_up: [travis/appveyor] Update compilers
    Problem:
    - travis is missing clang5/clang6/clang7 compilers.
    - appveyor is missing MSVC 2017 Preview compiler.
    
    Solution:
    - Update travis.yml and appveyor.yml with the newest versions of available compilers.

--- include/boost/sml/transition_table.hpp
+++ include/boost/sml/transition_table.hpp
@@ -55,7 +55,6 @@ typename front::state_sm<T>::type state __BOOST_SML_VT_INIT;
 #endif  // __pph__
 
 inline namespace literals {
-
 #if !defined(_MSC_VER)  // __pph__
 template <class T, T... Chrs>
 constexpr auto operator""_s() {
@@ -67,7 +66,7 @@ constexpr auto operator""_e() {
 }
 #endif  // __pph__
 
-} // literals
+}  // literals
 
 __BOOST_SML_UNUSED static front::state<back::terminate_state> X;
 __BOOST_SML_UNUSED static front::history_state H;
