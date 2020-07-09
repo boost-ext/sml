e71a6b6c39bb364c3c0339dd4bebe96d8f6e3e24
Kris Jusiak <kris@jusiak.net>
2020-07-09 07:46:04 -0600

    :wrench: [Conan] Update  package file

--- conanfile.py
+++ conanfile.py
@@ -3,15 +3,16 @@ import os
 from conans import ConanFile, tools
 
 
-class SmlConan(ConanFile):
+class SML(ConanFile):
     name = "SML"
     version = "latest"
+    url = "https://github.com/boost-ext/sml"
     license = "Boost"
-    url = "https://github.com/paulbendixen/sml.git"
     description = "[Boost::ext].SML: C++14 State Machine Library"
-    #no_copy_source = True
-    # No settings/options are necessary, this is header only
+    settings = "os", "compiler", "arch", "build_type"
     exports_sources = "include/*"
+    no_copy_source = True
 
     def package(self):
-        self.copy("*", src="include", dst="include")
+        self.copy("*.hpp")
+
