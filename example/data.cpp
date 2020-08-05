25a8d0e99e75015450f168022d4f9c279533e0a6
Kris Jusiak <kris@jusiak.net>
2020-08-05 11:00:24 -0600

    :art: :new: [example] Per state data
    Problem:
    - There is no example how to make data per state.
    
    Solution:
    - Extend data example to show how to make states with data.

--- example/data.cpp
+++ example/data.cpp
@@ -5,53 +5,63 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
-#if (__cplusplus >= 201703L && __has_include(<variant>))
 #include <boost/sml.hpp>
 #include <cassert>
 #include <iostream>
-#include <variant>
 
 namespace sml = boost::sml;
 
 namespace {
-struct connect {};
+struct connect {
+  int id{};
+};
 struct disconnect {};
+struct interrupt {};
+
+struct Disconnected {};
+struct Connected {
+  int id{};  /// per state data
+};
+struct Interrupted {
+  int id{};  /// per state data
+};
 
 class data {
-  struct Disconnected {};
-  struct Connected {
-    int id{};
-  };
-
  public:
   explicit data(const std::string& address) : address{address} {}
 
-  inline auto operator()() {
+  auto operator()() {
     using namespace boost::sml;
+
+    const auto set = [](const auto& event, Connected& state) { state.id = event.id; };
+
+    const auto update = [](Connected& src_state, Interrupted& dst_state) { dst_state.id = src_state.id; };
+
+    const auto print = [this](Connected& state) { std::cout << address << ':' << state.id << '\n'; };
+
     // clang-format off
     return make_transition_table(
-      * state<Disconnected> + event<connect> / [this] {
-          data_ = Connected{42};
-        } = state<Connected>
-      , state<Connected> + event<disconnect> / [this] {
-          std::cout << std::get<Connected>(data_).id << std::endl;
-        } = X
+      * state<Disconnected> + event<connect>    / (set, print)    = state<Connected>
+      , state<Connected>    + event<interrupt>  / (update, print) = state<Interrupted>
+      , state<Interrupted>  + event<connect>    / (set, print)    = state<Connected>
+      , state<Connected>    + event<disconnect> / (print)         = X
     );
     // clang-format on
   }
 
  private:
-  std::string address{};
-  std::variant<Disconnected, Connected> data_{};
+  std::string address{};  /// shared data between states
 };
 }  // namespace
 
 int main() {
-  sml::sm<data> sm{data{"127.0.0.1"}};
-  sm.process_event(connect{});
+  Disconnected ds{};
+  Connected cs{};
+  Interrupted is{};
+  sml::sm<data> sm{data{"127.0.0.1"}, ds, cs, is};
+  sm.process_event(connect{1024});
+  sm.process_event(interrupt{});
+  sm.process_event(connect{1025});
   sm.process_event(disconnect{});
   assert(sm.is(sml::X));
 }
-#else
-int main() {}
-#endif
