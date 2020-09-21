782f9f0b9ff58a4314694d8888c2878688e32918
Uy Ha <hchanuy@gmail.com>
2020-09-21 22:55:48 +0200

    Add license

--- test/ft/wrap.cpp
+++ test/ft/wrap.cpp
@@ -1,5 +1,9 @@
 //
-// Created by uyha on 21.09.2020.
+// Copyright (c) 2020-2020 Uy Ha
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
 //
 
 #include <boost/sml.hpp>
