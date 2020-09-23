9b45f91bf57ed271ef6000443ed814ea4e6eac68
Kris Jusiak <kris@jusiak.net>
2020-09-23 13:00:28 -0600

    :arrow_up: [copyright] Add  missing LICENSE info
    Problem:
    - All files are required to have LICENSE info.
    
    Solution:
    - Add LICENSE to the top missing files.

--- test/ft/actions_process_n_defer.cpp
+++ test/ft/actions_process_n_defer.cpp
@@ -1,3 +1,10 @@
+//
+// Copyright (c) 2016-2020 Kris Jusiak (kris at jusiak dot net)
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
 #include <boost/sml.hpp>
 #include <deque>
 #include <iostream>
