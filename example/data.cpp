5a7fd47701235cc4bee3edf69b0dac36dec2275e
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2023-02-16 18:49:53 +0100

    compiler error: adding a logger to the data.cpp example

--- example/data.cpp
+++ example/data.cpp
@@ -12,6 +12,35 @@
 namespace sml = boost::sml;
 
 namespace {
+struct printf_logger
+{
+  template <class SM, class TEvent>
+  void log_process_event(const TEvent&)
+  {
+    std::printf("[%s][event] %s\n", sml::aux::get_type_name<SM>(), sml::aux::get_type_name<TEvent>());
+  }
+
+  template <class SM, class TGuard, class TEvent>
+  void log_guard(const TGuard&, const TEvent&, bool result)
+  {
+    std::printf("[%s][guard] %s(%s): %s\n", sml::aux::get_type_name<SM>(), sml::aux::get_type_name<TGuard>(),
+                sml::aux::get_type_name<TEvent>(), (result ? "accept" : "reject"));
+  }
+
+  template <class SM, class TAction, class TEvent>
+  void log_action(const TAction&, const TEvent&)
+  {
+    std::printf("[%s][action] %s(%s)\n", sml::aux::get_type_name<SM>(), sml::aux::get_type_name<TAction>(),
+                sml::aux::get_type_name<TEvent>());
+  }
+
+  template <class SM, class TSrcState, class TDstState>
+  void log_state_change(const TSrcState& src, const TDstState& dst)
+  {
+    std::printf("[%s][transition] %s -> %s\n", sml::aux::get_type_name<SM>(), src.c_str(), dst.c_str());
+  }
+};
+
 struct connect {
   int id{};
 };
@@ -53,11 +82,14 @@ class data {
 
   std::string address{};  /// shared data between states
 };
+
 }  // namespace
+
+printf_logger logger;
 
 int main() {
   data d{std::string{"127.0.0.1"}};
-  sml::sm<data> sm{d, Connected{1}};
+  sml::sm<data, sml::logger<printf_logger>> sm{d, Connected{1}, logger};
   sm.process_event(connect{1024});
   sm.process_event(interrupt{});
   sm.process_event(connect{1025});
