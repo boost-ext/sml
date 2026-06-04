ba600ce0bb14be863f5ef2e2dfeb6d281920fba6
alexander-dengg <alexander.dengg@leica-geosystems.com>
2026-06-04 11:22:34 +0200

    cppcheck: suppress missingIncludeSystem

--- test/ft/non_owning_dependencies.cpp
+++ test/ft/non_owning_dependencies.cpp
@@ -5,6 +5,7 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
+// cppcheck-suppress missingIncludeSystem
 #include <boost/sml.hpp>
 
 namespace sml = boost::sml;
